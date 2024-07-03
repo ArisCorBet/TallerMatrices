2cdc9d1f4f9be4f10ede2a0cfb40db4ca4123440
ArisCorBet <ariana.cordova@unl.edu.ec>
2024-07-03 10:16:00 -0500

    Update 14Hexodecimal.c

---
+++ Programa1/TallerMatrices/14Hexodecimal.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("=============Matriz Cuadrada============== \n");
+
+    // Pedir al usuario el número de filas y columnas
+    printf("Ingrese el numero de filas y columnas: \n");
+    scanf("%d", &num);
+
+    int matriz1[num][num], matriz2[num][num], matrizsum[num][num];
+
+    // Pedir al usuario que ingrese los elementos de la primera matriz
+    printf("Ingrese los elementos de la primera matriz:\n");
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
+
+    // Pedir al usuario que ingrese los elementos de la segunda matriz
+    printf("Ingrese los elementos de la segunda matriz:\n");
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+
+    // Sumar las dos matrices
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
+            matrizsum[i][j] = matriz1[i][j] + matriz2[i][j];
+        }
+    }
+
+    // Mostrar la matriz resultante en hexadecimal
+    printf("La suma es:\n");
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
+            printf("%X ", matrizsum[i][j]);  // Usar %X para imprimir en hexadecimal
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
