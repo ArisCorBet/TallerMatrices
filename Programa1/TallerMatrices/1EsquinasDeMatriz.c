7e8882d75bfc7c5c202529cbb208d2482d5a2442
ArisCorBet <ariana.cordova@unl.edu.ec>
2024-07-03 09:27:59 -0500

    Update 1EsquinasDeMatriz.c

--- Programa1/TallerMatrices/1EsquinasDeMatriz.c
+++ Programa1/TallerMatrices/1EsquinasDeMatriz.c
@@ -17,7 +17,7 @@ int main(){
     //4  5   6     [1][0]  [1][1]  [1][2]  
     //7  8   9     [2][0]  [2][1]  [2][2]  
    
-
+    //presentar matriz con condicion
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
 
