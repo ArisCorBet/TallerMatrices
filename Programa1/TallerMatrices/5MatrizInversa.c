8c265fb5ee54f0e3324e3b612e50952124999651
ArisCorBet <ariana.cordova@unl.edu.ec>
2024-07-08 01:07:21 -0500

    Update 5MatrizInversa.c

--- Programa1/TallerMatrices/5MatrizInversa.c
+++ Programa1/TallerMatrices/5MatrizInversa.c
@@ -1,63 +1,71 @@
 #include <stdio.h>
 
-
-float determinante(int matriz[3][3]) {
-    float det = matriz[0][0] * (matriz[1][1] * matriz[2][2] - matriz[1][2] * matriz[2][1])
-              - matriz[0][1] * (matriz[1][0] * matriz[2][2] - matriz[1][2] * matriz[2][0])
-              + matriz[0][2] * (matriz[1][0] * matriz[2][1] - matriz[1][1] * matriz[2][0]);
-    return det;
-}
-
-void matrizAdjunta(int matriz[3][3], int adjunta[3][3]) {
-    adjunta[0][0] = (matriz[1][1] * matriz[2][2] - matriz[1][2] * matriz[2][1]);
-    adjunta[0][1] = -(matriz[1][0] * matriz[2][2] - matriz[1][2] * matriz[2][0]);
-    adjunta[0][2] = (matriz[1][0] * matriz[+
-    2][1] - matriz[1][1] * matriz[2][0]);
-    
-    adjunta[1][0] = -(matriz[0][1] * matriz[2][2] - matriz[0][2] * matriz[2][1]);
-    adjunta[1][1] = (matriz[0][0] * matriz[2][2] - matriz[0][2] * matriz[2][0]);
-    adjunta[1][2] = -(matriz[0][0] * matriz[2][1] - matriz[0][1] * matriz[2][0]);
-    
-    adjunta[2][0] = (matriz[0][1] * matriz[1][2] - matriz[0][2] * matriz[1][1]);
-    adjunta[2][1] = -(matriz[0][0] * matriz[1][2] - matriz[0][2] * matriz[1][0]);
-    adjunta[2][2] = (matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0]);
-}
-
-void inversaMatriz(int matriz[3][3], float inversa[3][3]) {
-    int adjunta[3][3];
-    matrizAdjunta(matriz, adjunta);
-    
-    float det = determinante(matriz);
-    if (det == 0) {
-        printf("La matriz no es invertible.\n");
-        return;
-    }
-    
+// Función para imprimir una matriz
+void imprimirMatriz(float matriz[3][3]) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            inversa[i][j] = adjunta[i][j] / det;
+            printf("%0.2f ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Función para encontrar la inversa de una matriz
+void invertirMatriz(float matriz[3][3], float inversa[3][3]) {
+    // Inicializamos la matriz inversa como la matriz identidad
+    // Si estamos en una posicion diagonal se imprime un 1 y si no se imprime un 0
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i == j) {
+                inversa[i][j] = 1;
+            } else {
+                inversa[i][j] = 0;
+            }
+        }
+    }
+
+    // Aplicamos el método de Gauss-Jordan
+    for (int i = 0; i < 3; i++) {
+        float temp = matriz[i][i];
+        
+        // Normalizamos la fila actual
+        for (int j = 0; j < 3; j++) {
+            matriz[i][j] /= temp;
+            inversa[i][j] /= temp;
+        }
+
+        // Eliminamos los elementos no deseados en las otras filas
+        for (int k = 0; k < 3; k++) {
+            if (k != i) {
+                temp = matriz[k][i];
+                for (int j = 0; j < 3; j++) {
+                    matriz[k][j] -= matriz[i][j] * temp;
+                    inversa[k][j] -= inversa[i][j] * temp;
+                }
+            }
         }
     }
 }
 
 int main() {
-    int matriz[3][3] = {
-        {1, 2, 3},
-        {0, 1, 4},
-        {5, 6, 0}
+    // Declaramos la matriz original y la matriz inversa
+    float matriz[3][3] = {
+        {2, -1, 0},
+        {-1, 2, -1},
+        {0, -1, 2}
     };
-    
     float inversa[3][3];
-    
-    inversaMatriz(matriz, inversa);
-    
-    printf("La matriz inversa es:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%f ", inversa[i][j]);
-        }
-        printf("\n");
-    }
-    
+
+    // Imprimimos la matriz original
+    printf("Matriz original:\n");
+    imprimirMatriz(matriz);
+
+    // Calculamos la inversa
+    invertirMatriz(matriz, inversa);
+
+    // Imprimimos la matriz inversa
+    printf("\nMatriz inversa:\n");
+    imprimirMatriz(inversa);
+
     return 0;
 }
