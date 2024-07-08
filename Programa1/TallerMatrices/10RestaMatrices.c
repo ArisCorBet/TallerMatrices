83d9110285e83f8edecd6e0ff249d40d45199b6d
ArisCorBet <ariana.cordova@unl.edu.ec>
2024-07-08 01:10:15 -0500

    Update 10RestaMatrices.c

--- Programa1/TallerMatrices/10RestaMatrices.c
+++ Programa1/TallerMatrices/10RestaMatrices.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
 
 int main() {
-    int filas, columnas;
+    int num;
+
+    
+    printf("=============Matriz Cuadrada============== \n");
 
     // Pedir al usuario el número de filas y columnas
-    printf("Ingrese el numero de filas: ");
-    scanf("%d", &filas);
-    printf("Ingrese el numero de columnas: ");
-    scanf("%d", &columnas);
+    printf("Ingrese el numero de filas y columnas: \n");
+    scanf("%d", &num);
+    
 
-    int matriz1[filas][columnas], matriz2[filas][columnas], matrizsum[filas][columnas];
+    int matriz1[num][num], matriz2[num][num], matrizsum[num][num];
 
     // Pedir al usuario que ingrese los elementos de la primera matriz
     printf("Ingrese los elementos de la primera matriz:\n");
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
             scanf("%d", &matriz1[i][j]);
         }
     }
 
     // Pedir al usuario que ingrese los elementos de la segunda matriz
     printf("Ingrese los elementos de la segunda matriz:\n");
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
             scanf("%d", &matriz2[i][j]);
         }
     }
 
-    // Sumar las dos matrices
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
+    // Restar las dos matrices
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
             matrizsum[i][j] = matriz1[i][j] - matriz2[i][j];
         }
     }
 
-    // Mostrar la matriz resultante
-    printf("La resta es:\n");
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
+    // Mostrar la matriz resultado de la resta
+    printf("La suma es:\n");
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
             printf("%d ", matrizsum[i] [j]);
         }
         printf("\n");
