f52459119ab2b4d119748b9b3a1bb8e2e1ea1abf
ArisCorBet <ariana.cordova@unl.edu.ec>
2024-07-08 01:06:16 -0500

    Update 3ElementosImpares.c

--- Programa1/TallerMatrices/3ElementosImpares.c
+++ Programa1/TallerMatrices/3ElementosImpares.c
@@ -4,7 +4,7 @@ int main(){
    
     printf("==========Matrices=========\n");
 
-    
+    //crear arreglo definido como matriz y definir valor de cada posicion
     int matriz [3][3];
     matriz[0][0]=1;
     matriz[0][1]=2;
@@ -19,11 +19,11 @@ int main(){
 
 ///////////////////////Elementos impares//////////////////
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++) // i para filas
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < 3; j++)  //j para columnas
                 {
-                    if (matriz[i][j] %2 == !0 )
+                    if (matriz[i][j] %2 == !0 ) //Condicion para solo imprimir los numeros pares
                     {
                         printf("Elementos impares: %d\n",matriz[i][j]);
                     }
