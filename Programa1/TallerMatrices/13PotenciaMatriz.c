6e4076f5e7d7717b5fb2cb706893ab37b8d1cbdf
ArisCorBet <ariana.cordova@unl.edu.ec>
2024-07-09 00:05:33 -0500

    Update 13PotenciaMatriz.c

--- Programa1/TallerMatrices/13PotenciaMatriz.c
+++ Programa1/TallerMatrices/13PotenciaMatriz.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 
-#define TAMANO 3 // Definimos el tamaño de la matriz (3x3 en este caso)
 
-void multiplicarMatrices(int primeraMatriz[TAMANO][TAMANO], int segundaMatriz[TAMANO][TAMANO], int resultado[TAMANO][TAMANO]) {
+void multiplicarMatrices(int primeraMatriz[3][3], int segundaMatriz[3][3], int resultado[3][3]) {
     // Función para multiplicar dos matrices
-    for (int i = 0; i < TAMANO; i++) {
-        for (int j = 0; j < TAMANO; j++) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             resultado[i][j] = 0; // Inicializamos el elemento en la matriz de resultado
-            for (int k = 0; k < TAMANO; k++) {
+            for (int k = 0; k < 3; k++) {
                 resultado[i][j] += primeraMatriz[i][k] * segundaMatriz[k][j]; // Sumamos el producto de los elementos correspondientes
             }
         }
     }
 }
 
-void copiarMatriz(int fuente[TAMANO][TAMANO], int destino[TAMANO][TAMANO]) {
+void copiarMatriz(int fuente[3][3], int destino[3][3]) {
     // Función para copiar una matriz en otra
-    for (int i = 0; i < TAMANO; i++) {
-        for (int j = 0; j < TAMANO; j++) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             destino[i][j] = fuente[i][j]; // Copiamos cada elemento de la matriz fuente a la matriz destino
         }
     }
 }
 
-void imprimirMatriz(int matriz[TAMANO][TAMANO]) {
+void imprimirMatriz(int matriz[3][3]) {
     // Función para imprimir una matriz
-    for (int i = 0; i < TAMANO; i++) {
-        for (int j = 0; j < TAMANO; j++) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             printf("%d ", matriz[i][j]); // Imprimimos cada elemento seguido de un espacio
         }
         printf("\n"); // Nueva línea después de cada fila
@@ -34,29 +33,25 @@ void imprimirMatriz(int matriz[TAMANO][TAMANO]) {
 }
 
 int main() {
-    int matriz[TAMANO][TAMANO] = {
+    int matriz[3][3] = {
         {2, 0, 1},
         {3, 0, 0},
         {5, 1, 1}
     }; // Nuestra matriz inicial (3x3)
 
-    int resultado[TAMANO][TAMANO]; // Matriz para almacenar el resultado
+    int resultado[3][3]; // Matriz para almacenar el resultado
     int potencia = 2; // Potencia a la que queremos elevar la matriz
-    int temporal[TAMANO][TAMANO]; // Matriz temporal para cálculos intermedios
+    int temporal[3][3]; // Matriz temporal para cálculos intermedios
 
     // Inicializamos la matriz resultante como la matriz identidad
-    for (int i = 0; i < TAMANO; i++) {
-        for (int j = 0; j < TAMANO; j++) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             if (i == j)
                 resultado[i][j] = 1; // La diagonal principal de la matriz identidad es 1
             else
                 resultado[i][j] = 0; // El resto de los elementos de la matriz identidad son 0
         }
     }
-
-    // Imprimimos la matriz original
-    printf("Matriz original:\n");
-    imprimirMatriz(matriz); // Llamamos a la función para imprimir la matriz original
 
     // Multiplicamos la matriz por sí misma tantas veces como sea necesario
     for (int p = 0; p < potencia; p++) {
@@ -68,5 +63,5 @@ int main() {
     printf("Matriz elevada a la potencia %d:\n", potencia);
     imprimirMatriz(resultado); // Llamamos a la función para imprimir la matriz resultante
 
-    return 0; // Fin del programa
+    return 0;
 }
