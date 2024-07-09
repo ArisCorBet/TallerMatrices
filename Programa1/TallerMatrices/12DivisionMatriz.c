5df5d187bf17b0338b03c6df0484f13806f0c9e0
ArisCorBet <ariana.cordova@unl.edu.ec>
2024-07-09 00:13:28 -0500

    Update 12DivisionMatriz.c

--- Programa1/TallerMatrices/12DivisionMatriz.c
+++ Programa1/TallerMatrices/12DivisionMatriz.c
@@ -1,43 +1,9 @@
 #include <stdio.h>
 
-void imprimirMatriz(int n, float matriz[n][n]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%f ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-int invertirMatriz2x2(float matriz[2][2], float inversa[2][2]) {
-    float determinante = matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0];
-    if (determinante == 0) {
-        return 0; // No tiene inversa si el determinante es 0
-    }
-    
-    float invDet = 1.0 / determinante;
-    inversa[0][0] = matriz[1][1] * invDet;
-    inversa[0][1] = -matriz[0][1] * invDet;
-    inversa[1][0] = -matriz[1][0] * invDet;
-    inversa[1][1] = matriz[0][0] * invDet;
-    
-    return 1;
-}
-
-void multiplicarMatrices2x2(float A[2][2], float B[2][2], float resultado[2][2]) {
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            resultado[i][j] = 0;
-            for (int k = 0; k < 2; k++) {
-                resultado[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-}
-
 int main() {
-    float A[2][2], B[2][2], B_inv[2][2], resultado[2][2];
+    float A[2][2], B[2][2], resultado[2][2];
     
+    // Ingreso de elementos para la matriz A (2x2)
     printf("Ingrese los elementos de la matriz A (2x2):\n");
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
@@ -45,6 +11,7 @@ int main() {
         }
     }
     
+    // Ingreso de elementos para la matriz B (2x2)
     printf("Ingrese los elementos de la matriz B (2x2):\n");
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
@@ -52,12 +19,33 @@ int main() {
         }
     }
     
-    if (invertirMatriz2x2(B, B_inv)) {
-        multiplicarMatrices2x2(A, B_inv, resultado);
+    // Calcular el determinante de B
+    float determinante = B[0][0] * B[1][1] - B[0][1] * B[1][0];
+    
+    // Verificar si B tiene inversa (determinante diferente de 0)
+    if (determinante != 0) {
+        // Calcular la matriz inversa de B
+        float invDet = 1.0 / determinante;
+        float B_inv[2][2];
+        
+        B_inv[0][0] = B[1][1] * invDet;
+        B_inv[0][1] = -B[0][1] * invDet;
+        B_inv[1][0] = -B[1][0] * invDet;
+        B_inv[1][1] = B[0][0] * invDet;
+        
+        // Multiplicar A por la inversa de B para obtener el resultado
+        resultado[0][0] = A[0][0] * B_inv[0][0] + A[0][1] * B_inv[1][0];
+        resultado[0][1] = A[0][0] * B_inv[0][1] + A[0][1] * B_inv[1][1];
+        resultado[1][0] = A[1][0] * B_inv[0][0] + A[1][1] * B_inv[1][0];
+        resultado[1][1] = A[1][0] * B_inv[0][1] + A[1][1] * B_inv[1][1];
+        
+        // Mostrar el resultado de la multiplicación A * B^-1
         printf("El resultado de A / B es:\n");
-        imprimirMatriz(2, resultado);
+        printf("%f %f\n", resultado[0][0], resultado[0][1]);
+        printf("%f %f\n", resultado[1][0], resultado[1][1]);
     } else {
-        printf("La matriz B no tiene inversa y, por lo tanto, la division no es posible.\n");
+        // Si B no tiene inversa
+        printf("La matriz B no tiene inversa y, por lo tanto, la división no es posible.\n");
     }
     
     return 0;
